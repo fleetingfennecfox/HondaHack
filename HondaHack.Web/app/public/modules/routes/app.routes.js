38298f8890d200ca11eb80e8b3d2ea15cb468456
Brent Ida <idabrent14@gmail.com>
2017-11-28 04:38:49 -0800

    Sending to master. Solved a few build errors.

--- HondaHack.Web/app/public/modules/routes/app.routes.js
+++ HondaHack.Web/app/public/modules/routes/app.routes.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     'use strict';
 
-    var app = angular.module(APP.NAME + '.routes', []);
+    var app = angular.module('publicApp' + '.routes', []);
 
     app.config(_configureStates);
 
@@ -11,36 +11,14 @@
         $locationProvider.html5Mode({
             enabled: true,
             requireBase: false
-        });
+        });        
 
-
-        //chained example;
         $stateProvider
             .state({
-                name: 'featureOne',
-                url: '/admin/featureOne',
-                templateUrl: '/app/admin/modules/feature-one/featureOne.html',
-                title: 'Feature One',
-                controller: 'featureOneController as f'
-
-            })
-            .state({
-                name: 'featureTwo',
-                component: 'featureTwo',
-                url: '/admin/featureTwo',
-                templateUrl: '/app/admin/modules/feature-two/featureTwo.html'
+                name: 'home',
+                url: '/public/home',
+                templateUrl: '/app/public/modules/homePage.html'
             });
-
-
-        //not chained example
-        var thirdFeature = {
-            name: 'featureThree',
-            component: 'featureThree',
-            url: '/admin/featureThree',
-            templateUrl: '/app/admin/modules/featureThree/featureThree.html'
-        };
-
-        $stateProvider.state(thirdFeature);
     }
 
 })();
